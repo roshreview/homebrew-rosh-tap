2acf40eae3d97931ceac23d18025a3151547d3c2
dcchuck <chuck.danielsson@gmail.com>
2019-01-30 14:25:04 -0500

    Remove lion from OS

--- Formula/percona-emqb.rb
+++ Formula/percona-emqb.rb
@@ -19,7 +19,7 @@ class PerconaServer < Formula
   # https://github.com/Homebrew/homebrew-core/issues/1475
   # Needs at least Clang 3.3, which shipped alongside Lion.
   # Note: MySQL themselves don't support anything below El Capitan.
-  depends_on :macos => :lion
+  depends_on :macos => :sierra
   depends_on "openssl"
 
   conflicts_with "mariadb", "mysql", "mysql-cluster",
